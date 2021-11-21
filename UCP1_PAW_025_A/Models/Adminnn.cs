870b3a0a6fd1783720ca5433bea5b3dfcab13465
Herlambang <muhammadherlambang9b@gmail.com>
2021-11-21 23:00:59 +0700

    Add project files.

---
+++ UCP1_PAW_025_A/Models/Adminnn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace UCP1_PAW_025_A.Models
+{
+    public partial class Adminnn
+    {
+        public string IdAdmin { get; set; }
+        public string NamaAdmin { get; set; }
+        public string AlamatAdmin { get; set; }
+    }
+}
---
+++ UCP1_PAW_025_A/Models/Barangg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace UCP1_PAW_025_A.Models
+{
+    public partial class Barangg
+    {
+        public string IdBarang { get; set; }
+        public string NamaBarang { get; set; }
+        public int? Harga { get; set; }
+        public string JenisBarang { get; set; }
+        public string UkuranBarang { get; set; }
+    }
+}
